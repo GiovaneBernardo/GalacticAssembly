f8e315340aa5a59c5e0f1010ba38de84955ea207
GiovaneBernardo <souzagiovane2019@gmail.com>
2025-05-07 00:54:34 -0300

    Updated camera to look more like space engineers

--- Assets/Scripts/CameraScript.cpp
+++ Assets/Scripts/CameraScript.cpp
@@ -6,6 +6,21 @@ namespace Plaza {
 
 	}
 
+    float ApplyDragToFloat(float value, const float drag) {
+		const float realDrag = glm::max(drag, value * drag);
+	    if (value >= 0.0f)
+	        return glm::max(0.0f, value - realDrag);
+
+	    return glm::min(0.0f, value + realDrag);
+	}
+
+    glm::vec3 ApplyDragToVec3(glm::vec3 value, const float drag) {
+	    return glm::vec3(
+            ApplyDragToFloat(value.x, drag),
+            ApplyDragToFloat(value.y, drag),
+            ApplyDragToFloat(value.z, drag));
+	}
+
     void CameraScript::OnUpdate(Scene* scene) {
         scene = Scene::GetActiveScene();
 
@@ -25,11 +40,29 @@ namespace Plaza {
         glm::quat newRotation = currentRotation * deltaRotation;
 
         // Move Player
-        if (Input::GetKeyDown(GLFW_KEY_W))
-            mVelocity += glm::vec3(0.0f, 0.0f, 1.0f * Time::GetDeltaTime());
+		auto& transform = *scene->GetComponent<TransformComponent>(playerUuid);
+		glm::quat rotation = transform.GetWorldQuaternion();
+		float dt = Time::GetDeltaTime();
+
+		if (Input::GetKeyDown(GLFW_KEY_W))
+			mVelocity += glm::rotate(rotation, glm::vec3(0.0f, 0.0f, 1.0f)) * dt;
+		if (Input::GetKeyDown(GLFW_KEY_S))
+			mVelocity += glm::rotate(rotation, glm::vec3(0.0f, 0.0f, -1.0f)) * dt;
+		if (Input::GetKeyDown(GLFW_KEY_D))
+			mVelocity += glm::rotate(rotation, glm::vec3(-1.0f, 0.0f, 0.0f)) * dt;
+		if (Input::GetKeyDown(GLFW_KEY_A))
+			mVelocity += glm::rotate(rotation, glm::vec3(1.0f, 0.0f, 0.0f)) * dt;
+		if (Input::GetKeyDown(GLFW_KEY_SPACE))
+			mVelocity += glm::rotate(rotation, glm::vec3(0.0f, 1.0f, 0.0f)) * dt;
+		if (Input::GetKeyDown(GLFW_KEY_C))
+			mVelocity += glm::rotate(rotation, glm::vec3(0.0f, -1.0f, 0.0f)) * dt;
+
+	    float drag = 0.8f * Time::GetDeltaTime();
+	    //mVelocity = ApplyDragToVec3(mVelocity, drag);
 
         ECS::TransformSystem::SetLocalRotation(*playerTransform, scene, newRotation);
         ECS::TransformSystem::SetLocalPosition(*playerTransform, scene, playerTransform->GetWorldPosition() + mVelocity);
         ECS::TransformSystem::UpdateSelfAndChildrenTransform(*scene->GetComponent<TransformComponent>(playerUuid), nullptr, scene, true);
+		mVelocity = glm::vec3(0.0f);
     }
 }
